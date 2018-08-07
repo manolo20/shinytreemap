0fb58fb0d2613a9c5fc4711dd322f75b7a76760e
Manolo Malaver-Vojvodic <mmala027@uottawa.ca>
2018-08-06 20:50:33 -0400

    first commit

---
+++ shinytreemap.R
@@ -0,0 +1,80 @@
+
+library(shiny)
+library(treemap)
+library(dplyr)
+library(gridBase)
+
+weights <- read.csv("/home/atai/Downloads/CPI dashboard/stackbar/data3_treemap.csv", stringsAsFactors = FALSE)
+
+weights <- weights %>% filter(!levels %in% "All-items") %>% 
+  filter(Ranking %in% c("1") & Region %in% c("Canada", "Ontario", "Quebec"))%>% 
+  select("Region", "Level.0", "Level.1", "X2015") 
+
+#saveRDS(weights,"/home/atai/Documents/R projects/treemap/stackoverflow/treemap1.rds")
+
+weights <- readRDS("/home/atai/Documents/R projects/treemap/stackoverflow/treemap1.rds")
+
+### Handle cliks on a treemap
+tmLocate <-
+  function(coor, tmSave) {
+    tm <- tmSave$tm
+    
+    # retrieve selected rectangle
+    rectInd <- which(tm$x0 < coor[1] & (tm$x0 + tm$w) > coor[1] &
+                       tm$y0 < coor[2] & (tm$y0 + tm$h) > coor[2])
+    
+    return(tm[rectInd[1], ])
+    
+  }
+
+ui = fluidPage(pageWithSidebar(
+  headerPanel("Interactive treemap"),
+  sidebarPanel(
+    selectInput("prov", "Select province:",choices = unique(weights$Region)), width = 2
+  ),
+  mainPanel(
+    plotOutput("plot", hover="hover", height = 700, width = 900), 
+    tableOutput("record")
+  )
+)
+)
+
+server = function(input, output){
+  sel1 <- reactive({
+    weights %>% filter(Region %in% input$prov)
+  })
+  
+  getRecord <- reactive({
+    x <- input$hover$x
+    y <- input$hover$y
+    
+    x <- (x - .tm$vpCoorX[1]) / (.tm$vpCoorX[2] - .tm$vpCoorX[1])
+    y <- (y - .tm$vpCoorY[1]) / (.tm$vpCoorY[2] - .tm$vpCoorY[1])
+    
+    
+    l <- tmLocate(list(x=x, y=y), .tm)
+    l[, 1:(ncol(l)-9)]            
+  })
+  
+  output$plot <- renderPlot({ 
+    #cat(input$hover$x, "\n")
+    par(mar=c(0,0,0,0), xaxs='i', yaxs='i') 
+    plot(c(0,1), c(0,1),axes=F, col="white")
+    vps <- baseViewports()
+    
+    .tm <<- treemap(sel1(), 
+                    index=c("Level.0","Level.1"),
+                    vSize="X2015", vp=vps$plot)
+    
+  })
+  output$record <- renderTable({
+    getRecord()
+  })
+}
+
+shinyApp(ui=ui, server=server)
+
+
+
+
+
